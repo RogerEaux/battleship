3aa8803ff650c28ecc33e79994b560da7f81a7ab
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-02-11 16:33:04 -0600

    Add function to handle ship hit

--- src/modules/ships.js
+++ src/modules/ships.js
@@ -4,14 +4,31 @@
 //  hitPoints
 //  sunk
 //  hit()
+
+//  Reduces hitPoints by 1
+//  If hitPoints is 0
+//    Set sunk to true
+//  Return hitPoints
+
 //  isSunk()
+
+//  Returns sunk
 
 export function createShip(length) {
   let shipLength = length;
+  let hitPoints = shipLength;
+
+  function hit() {
+    hitPoints -= 1;
+
+    return hitPoints;
+  }
 
   return {
     get length() {
       return shipLength;
     },
+
+    hit,
   };
 }
