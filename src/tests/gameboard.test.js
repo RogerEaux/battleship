4f47550477df267a44bb2b24c30d4c9027bc174b
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-02-11 19:17:53 -0600

    Test ship placed in given coords

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -2,10 +2,12 @@ import { createGameboard } from '../modules/gameboard';
 import { createShip } from '../modules/ships';
 
 let gameboard;
+let ship;
 let testGrid;
 
 beforeEach(() => {
   gameboard = createGameboard();
+  ship = createShip(3);
   testGrid = [];
   for (let i = 0; i < 10; i += 1) {
     testGrid.push([]);
@@ -15,5 +17,13 @@ beforeEach(() => {
 describe('Gameboard tests', () => {
   test('Generates grid correctly', () => {
     expect(gameboard.grid).toEqual(testGrid);
+  });
+
+  test('Ship is placed in given coords', () => {
+    gameboard.place(3, [0, 0]);
+    testGrid[0][0] = ship;
+    testGrid[1][0] = ship;
+    testGrid[2][0] = ship;
+    expect(gameboard.grid.toString()).toEqual(testGrid.toString());
   });
 });
