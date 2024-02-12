223b4899661a83e199c3603ac0553de78091f62c
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-02-11 23:05:09 -0600

    Add blindFire to attack random valid spot

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,30 +1,37 @@
 import { createGameboard } from './gameboard';
 
-//  attack(gameboard, coords)
-//  attackedSpots - Spots already attacked
-//  randomAttack(gameboard)
-
 export function createPlayer(name) {
   const playerName = name;
   const playerGameboard = createGameboard();
   const attackedSpots = [];
 
   function attack(enemyGameboard, coords) {
-    //  Check if coords are on previous attacked
-    //  Receive attack on enemy gameboard with coords
-    //
-    //  If attackedSpots includes coords
-    //    Return null
-    //  Gameboard receiveAttack with coords
-    //  Add coords to attackedSpots
-    //  Return true
-
     if (attackedSpots.includes(coords)) return null;
 
     enemyGameboard.receiveAttack(coords);
     attackedSpots.push(coords);
 
     return true;
+  }
+
+  function generateRandomCoords() {
+    const coords = [];
+
+    for (let i = 0; i < 2; i += 1) {
+      coords[i] = Math.floor(Math.random() * 10);
+    }
+
+    return coords;
+  }
+
+  function blindFire(enemyGameboard) {
+    let coords = generateRandomCoords();
+
+    while (attackedSpots.includes(coords)) {
+      coords = generateRandomCoords();
+    }
+
+    return attack(enemyGameboard, coords);
   }
 
   return {
@@ -37,5 +44,6 @@ export function createPlayer(name) {
     },
 
     attack,
+    blindFire,
   };
 }
