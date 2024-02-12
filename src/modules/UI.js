2a5c97104487931164ebb4e7f8de4209ec5be711
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-02-12 00:28:51 -0600

    Add createMain to create main

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -11,8 +11,25 @@ function createHeader() {
   return header;
 }
 
+function createMain() {
+  const main = document.createElement('main');
+  const playerOne = document.createElement('div');
+  const playerOneName = document.createElement('h2');
+  const playerTwo = document.createElement('div');
+  const playerTwoName = document.createElement('h2');
+
+  playerOne.classList = 'player playerOne';
+  playerTwo.classList = 'player playerTwo';
+
+  playerOne.append(playerOneName);
+  playerTwo.append(playerTwoName);
+  main.append(playerOne, playerTwo);
+
+  return main;
+}
+
 export function createContent() {
   const body = document.querySelector('body');
 
-  body.append(createHeader());
+  body.append(createHeader(), createMain());
 }
