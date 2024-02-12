27983e5abd54f69d17cbe41aa5beaa8bb2cf8caf
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-02-11 22:51:26 -0600

    Test enemy ships hit and missed

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -16,4 +16,18 @@ describe('Player tests', () => {
     expect(hero.attack(enemy.gameboard, [0, 0])).toEqual(true);
     expect(enemy.gameboard.grid[0][0]).not.toBeNull();
   });
+
+  test(`Enemy's ship is hit`, () => {
+    enemy.gameboard.place(3, [0, 0]);
+    hero.attack(enemy.gameboard, [0, 0]);
+
+    expect(enemy.gameboard.grid[0][0]).toStrictEqual(true);
+  });
+
+  test(`Enemy's ships are missed`, () => {
+    enemy.gameboard.place(3, [0, 0]);
+    hero.attack(enemy.gameboard, [3, 3]);
+
+    expect(enemy.gameboard.grid[3][3]).toStrictEqual(false);
+  });
 });
