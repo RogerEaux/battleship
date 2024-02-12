76596e256eecdff29f8346285cf8726d60faf255
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-02-12 00:29:21 -0600

    Add createBoard function
    Generates div board as an array analog with classes representing
    their index

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -11,6 +11,29 @@ function createHeader() {
   return header;
 }
 
+function createBoard() {
+  const board = document.createElement('div');
+
+  board.classList = 'board';
+
+  for (let i = 0; i < 10; i += 1) {
+    const col = document.createElement('div');
+
+    col.classList = `col col-${i}`;
+    for (let j = 0; j < 10; j += 1) {
+      const row = document.createElement('div');
+
+      row.classList = `row row-${j}`;
+
+      col.append(row);
+    }
+
+    board.append(col);
+  }
+
+  return board;
+}
+
 function createMain() {
   const main = document.createElement('main');
   const playerOne = document.createElement('div');
@@ -21,8 +44,8 @@ function createMain() {
   playerOne.classList = 'player playerOne';
   playerTwo.classList = 'player playerTwo';
 
-  playerOne.append(playerOneName);
-  playerTwo.append(playerTwoName);
+  playerOne.append(playerOneName, createBoard());
+  playerTwo.append(playerTwoName, createBoard());
   main.append(playerOne, playerTwo);
 
   return main;
