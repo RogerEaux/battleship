da5ee01723decaf4938a7d6444deaf941823caa5
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-02-11 22:46:30 -0600

    Add attack method
    Sends attack to enemy gameboard at given coords

--- src/modules/player.js
+++ src/modules/player.js
@@ -5,11 +5,37 @@ import { createGameboard } from './gameboard';
 //  randomAttack(gameboard)
 
 export function createPlayer(name) {
-  let playerName = name;
+  const playerName = name;
+  const playerGameboard = createGameboard();
+  const attackedSpots = [];
+
+  function attack(enemyGameboard, coords) {
+    //  Check if coords are on previous attacked
+    //  Receive attack on enemy gameboard with coords
+    //
+    //  If attackedSpots includes coords
+    //    Return null
+    //  Gameboard receiveAttack with coords
+    //  Add coords to attackedSpots
+    //  Return true
+
+    if (attackedSpots.includes(coords)) return null;
+
+    enemyGameboard.receiveAttack(coords);
+    attackedSpots.push(coords);
+
+    return true;
+  }
 
   return {
     get name() {
       return playerName;
     },
+
+    get gameboard() {
+      return playerGameboard;
+    },
+
+    attack,
   };
 }
